74c0a520b6f29f704028d2d0a03ea5be77e0db66
bonatoneto <joaoadolfobonato@gmail.com>
2023-01-27 17:07:49 -0300

    js
    js

--- desafios/des02/script.js
+++ desafios/des02/script.js
@@ -1,17 +1,23 @@
+
 function carregar() {
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
+    var msg2 = document.getElementById('msg2')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+
     if ((hora => 5) && (hora < 12)) {
         img.src = 'imagens/dia.png'
         document.body.style.background = '#70a8c3'
+        msg2.innerHTML = 'Bom dia!'
     } else if ((hora > 12) && (hora <= 18)) {
         img.src = 'imagens/tarde.png'
         document.body.style.background = '#f5bb6a'
+        msg2.innerHTML = 'Boa tarde!'
     } else if ((hora > 18) && (hora < 24)) {
         img.src = 'imagens/noite.png'
         document.body.style.background = '#041020'
+        msg2.innerHTML = 'Boa noite!'
     }
 }
--- desafios/des03/script.js
+++ desafios/des03/script.js
@@ -1,3 +1,17 @@
 function verificar() {
-    alert('Hello Word')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nasc = document.getElementById('txtano')
+    var mensagem = document.getElementById('mensagem')
+    var mensagem2 = document.getElementById('mensagem2')
+
+    if ((nasc.value == 0) || Number(nasc.value > ano)) {
+        window.alert('Verifique os dados e tente novamente')
+    } else {
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(nasc.value)
+        mensagem.innerHTML = `Voce nasceu em ${nasc.value}`
+        mensagem2.innerHTML = `E tem ${idade} anos!`
+    }
+
 }
